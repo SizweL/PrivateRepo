cde8bf5e8c29bce6592080226a9304ad1c0ed57f
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-25 21:40:29 +0200

    Try 9

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/process_get', function (req, res) {
    res.end(JSON.stringify(response));
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
