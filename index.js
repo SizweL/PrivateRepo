12ffe17a1819f4302b46c3624932196d2adcc610
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-25 21:27:14 +0200

    switched back to server

--- index.js
+++ index.js
@@ -6,25 +6,15 @@ let path = require('path');
 let mainRouter = express.Router();
 
 
-/*var server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
-});*/
+});
 
-app.get('/index', function(req, res){
- res.sendFile(path.join(__dirname, 'index.html'));
- });
 
-mainRouter.get('/', function (req, res) {
- res.send('Hello World');
- });
-
- mainRouter.get('/', function(req, res){
- res.sendFile(path.join(__dirname, 'views', 'about.html'));
- });
 var port = process.env.PORT || 1337;
-app.listen(port);
+server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
