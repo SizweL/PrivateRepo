9f5e73da8b16d433482da4d42319032f4380b7ab
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 01:49:24 +0200

    Storing user search value

---
+++ js/User-Search-Storage.js
@@ -0,0 +1,6 @@
+function UserSearch(){
+const button = document.getElementById('UserInput');
+button.addEventListener('click', function(e) {
+  document.getElementById('counter').innerHTML = "Search for " + button + " Textbook";
+})
+}
