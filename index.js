b44e6b0d22798ab07ff96a8a5dc6df76602df5d3
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-25 21:22:07 +0200

    try somehting else 5

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ let path = require('path');
 
 let mainRouter = express.Router();
 
+
+/*var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});*/
+
 app.get('/index', function(req, res){
  res.sendFile(path.join(__dirname, 'index.html'));
  });
 
+mainRouter.get('/', function (req, res) {
+ res.send('Hello World');
+ });
 
+ mainRouter.get('/about', function(req, res){
+ res.sendFile(path.join(__dirname, 'views', 'about.html'));
+ });
 var port = process.env.PORT || 1337;
 app.listen(port);
 
