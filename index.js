4fdbfb898ff6b1a7eb5d27001b6e9dc3777b9838
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-25 20:47:45 +0200

    Try something else 3

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var http = require('http');
+let express = require('express');
+let app = express();
+let path = require('path');
+let mainRouter = express.Router();
+
 
 var server = http.createServer(function(request, response) {
 
@@ -6,6 +11,11 @@ var server = http.createServer(function(request, response) {
     response.end("Hello World!");
 
 });
+
+app.get('/about', function(req, res){
+ res.sendFile(path.join(__dirname, 'index.html'));
+ });
+
 
 var port = process.env.PORT || 1337;
 server.listen(port);
