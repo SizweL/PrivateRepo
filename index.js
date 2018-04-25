b5f03300834ba46f29b47205c04d00f045d820a2
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-25 21:55:09 +0200

    try 10

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-
-})
+app.listen(process.env.PORT || 1337);
