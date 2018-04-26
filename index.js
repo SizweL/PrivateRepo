f6d96e880797b832c212e74ff8cbacb84d703c4b
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 05:31:49 +0200

    added roouter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var search_path = require('./User-Search-Storage.js');
+let mainRouter = express.Router();
+
 
 app.use('/base_url', app);
 app.use(express.static(path.join(__dirname, 'js')));
@@ -11,6 +13,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+mainRouter.get('/about', function(req, res){
+ res.sendFile(path.join(__dirname, 'js', 'User-Search-Storage.js'));
+ });
 
+module.exports = mainRouter;
 
 app.listen(process.env.PORT || 1337);
