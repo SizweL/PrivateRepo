07517072981eb7cfb75cc8328c5393156156658a
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 06:11:34 +0200

    fault_7

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-let todoRouter = require("./User-Search-Storage.js");
 
 
 // viewed at http://localhost:8080
@@ -9,6 +8,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use("/js", todoRouter);
+
 
 app.listen(process.env.PORT || 1337);
