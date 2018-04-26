0b4c85a6c62d65652b5f2db508400b66cf43b738
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 05:38:07 +0200

    fault

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var search_path = require('./User-Search-Storage.js');
 let mainRouter = express.Router();
 
 
-app.use(express.static(path.join(__dirname, 'js')));
 
 
 // viewed at http://localhost:8080
@@ -12,9 +11,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-mainRouter.get('/about', function(req, res){
- res.sendFile(path.join(__dirname, 'js', 'User-Search-Storage.js'));
- });
 
 
 
