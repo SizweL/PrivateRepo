17bcaeddcc2e0816523c19754c6069632fed77f8
Sizwe Lekoba <lekobas@DLAB63.EIEUG>
2018-04-26 16:03:00 +0200

    dsadf

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+let bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let mainRouter=require('./User-Search-Storage.js');
+app.use('/cdn', express.static('public'));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
