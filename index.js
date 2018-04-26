a64a3a101c02040d15e627ae41d1ecc3e4e6e98d
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 04:38:53 +0200

    damm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-let SearchRouter = require("./User-Search-Storage.js");
+
 
 app.use(express.static('js'))
 // viewed at http://localhost:8080
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
- app.use("/", SearchRouter);
+
 
 app.listen(process.env.PORT || 1337);
