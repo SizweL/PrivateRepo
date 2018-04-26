f6aa2e86ea27adb2c1fd6c9de13108c3f8ddfc53
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 05:43:57 +0200

    fault_4

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use('/base_url', app);
-app.use(express.static(path.join(__dirname, 'js')));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
