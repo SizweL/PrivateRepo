ff0a57ac66c574ae58cf2a8e06aca70da7a16391
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 05:20:38 +0200

    that boy young

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+var search_path = require('./User-Search-Storage.js');
 
-
+app.use('/base_url', app);
 app.use(express.static(path.join(__dirname, 'js')));
+app.use("/js", search_path);
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
