ee664e247ee7d983710be6fa10a61447edae4613
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 04:36:33 +0200

    qee

--- User-Search-Storage.js
+++ User-Search-Storage.js
@@ -1,5 +1,6 @@
-let button = document.getElementById('helloButton');
-2
-3 button.addEventListener('click', function() {
-4 alert( "Hello World!" );
-5 }, false);
+function UserSearch(){
+const button = document.getElementById('UserInput');
+button.addEventListener('click', function(e) {
+  document.getElementById('counter').innerHTML = "Search for " + button + " Textbook";
+})
+}
--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
+ app.use("/", SearchRouter);
 
 app.listen(process.env.PORT || 1337);
