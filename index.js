3b5eec7826db1da2a654b3e6719eab3c2788a803
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 04:46:01 +0200

    Back to trying

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 
-app.use(express.static('js'))
+app.use(express.static(path.join(__dirname, 'js')));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
