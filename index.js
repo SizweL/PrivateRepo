7bfe67d053725fa1f828b83bda148eabd7dd0048
Sizwe Lekoba <lekobas@DLAB59.EIEUG>
2018-04-26 03:08:07 +0200

    changed again kjf

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var app = express();
 var path = require('path');
 let SearchRouter = require("./User-Search-Storage.js");
 
-
+app.use(express.static('js'))
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
- app.use("/", SearchRouter);
+
 
 app.listen(process.env.PORT || 1337);
