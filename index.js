bcf65e67e20392ddffa1f895eb23462dcb4dd829
SizweL <793314@students.wits.ac.za>
2018-05-02 18:21:39 +0200

    start

--- index.js
+++ index.js
@@ -1,95 +1,67 @@
+const {google} = require('googleapis');
 
-var express = require('express');
-var app=express();
-var bodyParser= require('body-parser');
-var jwt=require('jsonwebtoken');
+let path = require('path');
+let express = require('express');
+let mainRouter = require('./mainRouter.js');
+let viewsRouter = require('./viewsRoutes.js');
+let actionsRouter = require('./BookSiteActions.js');
 
-var users=[
-{
-  name:"xxxx",
-  password:"xxxx"
-},
-{
-  name:"yyyy",
-  password:"yyyy"
-}
-]
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+let bodyParser = require('body-parser');
 
-app.use(express.static('./'));
+let app = express();
+app.use('/',mainRouter);
+app.use('/views',viewsRouter);
+app.use('/actions',actionsRouter);
+app.use(express.static('static'));
 
-app.get('/', (req,res)=>{
-    res.sendFile('index.html');
+
+app.use('/cdn', express.static('json'));
+app.use('/cdn2', express.static('images'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
-app.post('/login',(req,res)=>{
-    var message;
-    for(var user of users){
-      if(user.name!=req.body.name){
-          message="Wrong Name";
-      }else{
-          if(user.password!=req.body.password){
-              message="Wrong Password";
-              break;
-          }
-          else{
-            //create the token.
-              var token=jwt.sign(user,"samplesecret");
-              message="Login Successful";
-              break;
-          }
-      }
+app.listen(process.env.PORT || 3000);
+console.log("Express server running on port 3000");
+
+const OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2("1071667068495-s0u1944ep3aol103in0d8pqfl8383f5c.apps.googleusercontent.com", "5ptbdJU4Mo96UtGYyt5mvzXn", "https://bantutextbooks.azurewebsites.net/search");
+
+// generate a url that asks permissions for Google+ and Google Calendar scopes
+var scopes = [
+  'https://www.googleapis.com/auth/gmail.modify'
+];
+
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+  scope: scopes // If you only need one scope you can pass it as string
+});
+
+app.get("/url", function(req, res) {
+  res.send(url);
+});
+
+app.get("/tokens", function(req, res) {
+
+  var code = req.query.code;
+
+  console.log(code);
+
+  oauth2Client.getToken(code, function(err, tokens) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+      return;
     }
-    //If token is present pass the token to client else send respective message
-    if(token){
-        res.status(200).json({
-            message,
-            token
-        });
-    }
-    else{
-        res.status(403).json({
-            message
-        });
-    }
-});
 
-app.use((req, res, next)=>{
-        // check header or url parameters or post parameters for token
-        var token = req.body.token || req.query.token || req.headers['x-access-token'];
-        if(token){
-          //Decode the token
-          jwt.verify(token,"samplesecret",(err,decod)=>{
-            if(err){
-              res.status(403).json({
-                message:"Wrong Token"
-              });
-            }
-            else{
-              //If decoded then call next() so that respective route is called.
-              req.decoded=decod;
-              next();
-            }
-          });
-        }
-        else{
-          res.status(403).json({
-            message:"No Token"
-          });
-        }
-});
+    console.log("allright!!!!");
 
-app.post('/getusers',(req,res)=>{
-    var user_list=[];
-    users.forEach((user)=>{
-        user_list.push({"name":user.name});
-    })
-    res.send(JSON.stringify({users:user_list}));
-});
+    console.log(err);
+    console.log(tokens);
+    oauth2Client.setCredentials(tokens);
 
-app.listen(process.env.PORT ||3000, function(){
-  console.log('listening on port 3000');
+    res.send(tokens);
+  });
 });
