34f163115e25f39ef43680392b47403294b8aa6b
SizweL <793314@students.wits.ac.za>
2018-05-04 01:59:49 +0200

    Start up

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engin', 'ejs');
+
 app.get('/', function (req, res) {
 
-  res.send('Hello World!');
+  res.render('index');
 });
 //the server is listening on port 3000 for connections
 app.listen(process.env.PORT || 3000, function () {
