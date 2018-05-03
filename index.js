7b21639ee30884f704e765eb5e96bb0a0a546697
SizweL <793314@students.wits.ac.za>
2018-05-04 01:51:28 +0200

    small start

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-//loading modules
-let path = require('path');
-let express = require('express');
-let mainRouter = require('./mainRoutes.js');
-let todoRouter = require('./todoRoutes.js');
-let bodyParser = require('body-parser');
 
-let app = express();
-//use body-parser for JSON and URL encoded form bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+var express = require('express');
 
-//mounting our routers
-app.use('/',mainRouter);
-app.use('/todo',todoRouter);
-app.use('/cdn', express.static('public'));
+var app = express();
 
-		
-		
-app.listen(process.env.PORT || 3000);
-console.log("Express server running on port 3000");
+app.get('/', function (req, res) {
+
+  res.send('Hello World!');
+});
+//the server is listening on port 3000 for connections
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+});
