4cdb4701312f3ab3c24712ff84f92b032a91a397
SizweL <793314@students.wits.ac.za>
2018-05-04 01:21:53 +0200

    Lab3

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const {google} = require('googleapis');
-
+//loading modules
 let path = require('path');
 let express = require('express');
-let mainRouter = require('./mainRouter.js');
-let viewsRouter = require('./viewsRoutes.js');
-let actionsRouter = require('./BookSiteActions.js');
-
+let mainRouter = require('./mainRoutes.js');
+let todoRouter = require('./todoRoutes.js');
 let bodyParser = require('body-parser');
 
 let app = express();
+//use body-parser for JSON and URL encoded form bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//mounting our routers
 app.use('/',mainRouter);
-app.use('/views',viewsRouter);
-app.use('/actions',actionsRouter);
-app.use(express.static('static'));
+app.use('/todo',todoRouter);
+app.use('/cdn', express.static('public'));
 
-
-app.use('/cdn', express.static('json'));
-app.use('/cdn2', express.static('images'));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
-});
-
+		
+		
 app.listen(process.env.PORT || 3000);
 console.log("Express server running on port 3000");
