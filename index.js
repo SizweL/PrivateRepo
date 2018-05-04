515dff85efa33a19c1c1dd38cbd37abf6a327a59
SizweL <793314@students.wits.ac.za>
2018-05-04 08:09:24 +0200

    adding price23

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/removetask", function(req, res) {
 
 //render the ejs and display added task, completed task
 app.get("/", function(req, res) {
-    res.render("index", { task: task, PriceEstimate:PriceEstimate, Quantity:Quantity complete: complete });
+    res.render("index", { task: task, PriceEstimate:PriceEstimate, Quantity:Quantity, complete: complete });
 });
 
 //set app to listen on port 3000
