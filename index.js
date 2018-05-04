2a280ff28d649650d30f25c5977df781e0bf50b8
SizweL <793314@students.wits.ac.za>
2018-05-04 02:27:23 +0200

    Adding items

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ var express = require('express');
 
 var app = express();
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
 
   res.render('index');
+});
+
+app.post('/addtask', function (req, res) {
+   res.render('index')
+});
+
+//the task array with initial placeholders for added task
+var task = ["buy socks", "practise with nodejs"];
+//post route for adding new task
+app.post('/addtask', function (req, res) {
+    var newTask = req.body.newtask;
+//add the new task from the post route into the array
+    task.push(newTask);
+//after adding to the array go back to the root route
+    res.redirect("/");
+});
+//render the ejs and display added task, task(index.ejs) = task(array)
+app.get("/", function(req, res) {
+    res.render("index", { task: task});
 });
 //the server is listening on port 3000 for connections
 app.listen(process.env.PORT || 3000, function () {
