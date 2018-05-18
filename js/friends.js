6652e6705d367cfdca4e6464bb9a1a510fc7a307
SizweL <793314@students.wits.ac.za>
2018-05-18 20:37:46 +0200

    trying lint

--- js/friends.js
+++ js/friends.js
@@ -1,49 +1,52 @@
-function showFriends(){
-    var xhr = new XMLHttpRequest();
-	xhr.open("GET", "users.json", true);
-	let requestErrorFunc = function() {
- alert( "ERROR: failed to load todoList" );
- };
-    xhr.onload = function() {
-       
-       if (xhr.status >= 200 && xhr.status < 400) {     
-	   var x = document.createElement("SELECT");
-		x.setAttribute("id", "friends");
-		x.setAttribute("name", "friends");
-		var element = document.getElementById("sharing");
-		element.appendChild(x);
-		//document.body.appendChild(x);
-		
-		var z = document.createElement("option");
-		var t = document.createTextNode("select friend");
-		z.appendChild(t);
-		document.getElementById("friends").appendChild(z);
-		var x = document.getElementById("friends").options[0].disabled = true;
-		var obj = JSON.parse(xhr.responseText);
-		let array = obj.users;
-		
-		array.forEach(function(user){
-			
-				//document.getElementById("friends").innerHTML = '<option value="'+user.UserName+'">'+user.FirstName+" "+user.LastName+'</option>';
-				var z = document.createElement("option");
-				z.setAttribute("value", user.UserName);
-				var t = document.createTextNode(user.FirstName+" "+user.LastName);
-				z.appendChild(t);
-				document.getElementById("friends").appendChild(z);
-			});
-			
-		var btn = document.createElement("INPUT");
-		btn.setAttribute("type", "submit");
-		var element = document.getElementById("sharing");
-		element.appendChild(btn);
+'use strict';
+/*eslint no-unused-vars: ["error", { "vars": "local" }]*/
+/* global XMLHttpRequest, alert, document */
+/* exported y */
+function showFriends(){ // eslint-disable-line no-unused-vars
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'users.json', true);
+  var requestErrorFunc = function() {
+    alert('ERROR: failed to load todoList');
+  };
+  xhr.onload = function() {
+
+    if (xhr.status >= 200 && xhr.status < 400) {
+      var x = document.createElement('SELECT');
+      x.setAttribute('id', 'friends');
+      x.setAttribute('name', 'friends');
+      var element = document.getElementById('sharing');
+      element.appendChild(x);
+      // document.body.appendChild(x);
+
+      var z = document.createElement('option');
+      var t = document.createTextNode('select friend');
+      z.appendChild(t);
+      document.getElementById('friends').appendChild(z);
+      var y = document.getElementById('friends').options[0].disabled = true; // eslint-disable-line no-unused-vars
+      var obj = JSON.parse(xhr.responseText);
+      var array = obj.users;
+
+      array.forEach(function(user){
+
+
+        var z = document.createElement('option');
+        z.setAttribute('value', user.UserName);
+        var t = document.createTextNode(user.FirstName + ' ' + user.LastName);
+        z.appendChild(t);
+        document.getElementById('friends').appendChild(z);
+      });
+
+      var btn = document.createElement('INPUT');
+      btn.setAttribute('type', 'submit');
+      var element_ = document.getElementById('sharing');
+      element_.appendChild(btn);
+    } else {
+      requestErrorFunc();
+
     }
-	else {
-		requestErrorFunc();
-	}
-    
-	
-		
-	
+
+
+  };
+  xhr.send();
 }
-xhr.send();
-}
+
